01f5b7d2f6fc84281d310e3b690f5989b45ee816
lamazoshvilil <lamazoshvilil7@gmail.com>
2024-07-09 21:37:10 +0400

    js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-let navigation = document.getElementById(navBar);
-let burger = document.getElementById (burgerBar);
+let navigation = document.getElementById('navBar');
+let burger = document.getElementById ('burgerBar');
 
 
 burger.addEventListener('click', function() {
